c015c2cfedd5ef4f4ebbde4399449e773bb1c441
王国桥 <wfdwgq2013@126.com>
2022-11-17 14:11:49 +0800

    first commit

---
+++ fillDoris.js
@@ -0,0 +1,27 @@
+
+let fillScript = function () {
+    elements = document.getElementsByClassName('mtd-input');
+    num = document.getElementById('fillInput').value;
+    const eInput = new Event('input');
+    elements.forEach(element => {
+        if (element.placeholder == '长度') {
+            element.value = num;
+            element.dispatchEvent(eInput);
+        }
+    });
+}
+
+var fillScriptText = fillScript.toLocaleString()
+var fillScriptText = fillScriptText.substring(14, fillScriptText.length - 1)
+
+function update() {
+    setTimeout(() => {
+        $('body > div.app-main > section > section > section > div > div.mtd-loading-nested > div > div:nth-child(4)').append('<input id="fillInput"></input><button onclick="' + fillScriptText + '">fill</button>')
+    }, 500);
+}
+
+// update()
+
+$('body > div.app-main > section > section > section > div > div.mtd-loading-nested > div > div.sync > div.sync-title > i').click(function () {
+    update()
+})
