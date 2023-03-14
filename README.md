8f584514bba6723ab66d403c0f11f71fc6a0e1af
王国桥 <wfdwgq2013@126.com>
2023-03-14 16:18:40 +0800

    add python script

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # SQLBoyHelper
- 
-Doris  
-![图片](/img/doris1.png)  
-有时需要刷新页面  
-  
-魔数  
+
+## chrome插件
+路径【/chrome extension】是chrome插件，目前支持魔数、Doris编辑模型
+
+### Doris  
+![图片](/chrome extension/img/doris1.png)  
+有时需要刷新页面
+
+### 魔数  
 只支持首标签  
-  
+
+
---
+++ sql generators/gen_view.py
@@ -0,0 +1,53 @@
+import sys
+
+
+def print_new_line(sql_type, index, comma):
+    if sql_type == 1:
+        print('    ' + comma + '`' + field_name[index] + '`'
+              + ' ' * (max_field_name_length - len(field_name[index]) + 1) + 'COMMENT'
+              + ' \'' + comment[index] + '\'')
+    elif sql_type == 2:
+        print('    ' + comma + '`' + field_name[index] + '`')
+
+
+if __name__ == '__main__':
+    field_name = []
+    comment = []
+    field_type = []
+    max_field_name_length = 0
+    for line in sys.stdin:
+        if line == '\n':
+            print('========== 以下为SQL输出 ==========')
+            print()
+            print('DROP VIEW IF EXISTS `$target.table`;')
+            print('CREATE VIEW IF NOT EXISTS `$target.table`')
+            print('(')
+            print_new_line(1, 0, ' ')
+            for i in range(1, len(field_name)):
+                print_new_line(1, i, ',')
+            print(') COMMENT \'\'')
+            print('AS')
+            print('select')
+            print_new_line(2, 0, ' ')
+            for i in range(1, len(field_name)):
+                print_new_line(2, i, ',')
+            print('from ;')
+            sys.exit(0)
+        line_splited = line.split('\n')[0].split('\t')
+        l1 = line_splited[0]
+        l2 = line_splited[1]
+        l3 = line_splited[2]
+        # 做一点处理
+        if l1 == '字段名' or '_update_timestamp' in l1:
+            continue
+        elif '[P]' in l1:
+            l1 = l1.split(' ')[0]
+        if l1 == 'id' and l2 == '':
+            l2 = '主键'
+        # 记录最大长度
+        if len(l1) > max_field_name_length:
+            max_field_name_length = len(l1)
+        # 写入数组
+        field_name.append(l1)
+        comment.append(l2)
+        field_type.append(l3)
